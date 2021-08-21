e9eb15edb2e3efd6d58d2b60405823bfc96dbc55
janeyrinn <jane.rinn@gmail.com>
2021-08-21 13:53:33 +0000

    Added cart context and delivery logic

--- karmayoga/settings.py
+++ karmayoga/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context.cart_contents',
             ],
         },
     },
@@ -153,6 +154,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FREE_DELIVERY_THRESHOLD = 60
+STANDARD_DELIVERY_PERCENTAGE = 15
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
