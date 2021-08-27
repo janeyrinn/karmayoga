c92ea7c5b7a43b81bb6338e96ba259f3b62cea7a
janeyrinn <jane.rinn@gmail.com>
2021-08-27 09:42:31 +0000

    Added profile form, views and updated templates

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#f29188');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#f29188');
+    } else {
+        $(this).css('color', '#262626');
+    }
+});
