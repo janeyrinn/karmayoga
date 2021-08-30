de9c41f5e1a27a2ba2ea4b75da41d2c96a1dcba4
janeyrinn <jane.rinn@gmail.com>
2021-08-30 19:50:00 +0000

    Remove timetable, add clothing links

--- karmayoga/settings.py
+++ karmayoga/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
-    'timetable',
     'profiles',
 
     # Other
--- timetable/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def view_timetable(request):
-    """ A view to return the timetable template """
-
-    return render(request, 'timetable/timetable.html')
