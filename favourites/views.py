bc74161db025f4674cd8e8683efbbb70ced4ccf3
janeyrinn <jane.rinn@gmail.com>
2021-09-04 18:41:06 +0000

    split the add function in to two add/remove

--- favourites/views.py
+++ favourites/views.py
@@ -2,25 +2,22 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Favourites
 from products.models import Product
+from .models import Favourites
 
 
 @login_required
 def view_favourites(request):
     """A view to display a users favourites"""
 
-    favourites = None
-    try:
-        favourites = Favourites.objects.get(user=request.user)
-    except Favourites.DoesNotExist:
-        pass
+    favourites = Favourites.objects.get(user=request.user)
 
-        context = {
+    template = 'favourites/favourites.html'
+    context = {
             'favourites': favourites,
         }
 
-    return render(request, 'favourites/favourites.html', context)
+    return render(request, template, context)
 
 
 @login_required
@@ -28,16 +25,28 @@ def add_favourite(request, product_id):
     """Adds a product to the users 'Favourites' """
 
     product = get_object_or_404(Product, pk=product_id)
+
     # Creates the users 'favourites' container if one doesn't exist
     favourites, _ = Favourites.objects.get_or_create(user=request.user)
 
-    if product in favourites:
-        favourites.products.remove(product)
-        messages.success(request, 'Product removed from favourites')
-    else:
-        # Adds the selected product to the favourites
-        favourites.products.add(product)
-        messages.success(request, f'{product.name}\
-            was added to your saved items')
+    # Adds the selected product to the favourites
+    favourites.products.add(product)
+    messages.success(request, f'{product.name}\
+        added to favourites')
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def remove_favourite(request, product_id):
+    """Removes a product to the users 'Favourites' """
+
+    product = get_object_or_404(Product, pk=product_id)
+    favourites = Favourites.objects.get(user=request.user)
+
+    # Removes the selected product from favourites
+    favourites.products.remove(product)
+    messages.info(request, f'{product.name}\
+        removed from favourites')
 
     return redirect(request.META.get('HTTP_REFERER'))
