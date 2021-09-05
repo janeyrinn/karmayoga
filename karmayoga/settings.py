825c5ce18069650e4fea9de6a3a766df789a544b
janeyrinn <jane.rinn@gmail.com>
2021-09-05 17:05:16 +0000

    Removed secret key and set debug

--- karmayoga/settings.py
+++ karmayoga/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#wcoouh7tp+nqk&su6_7(&ds07%5lbj7u%u3r85nvdk$&1g^$7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['karmayoga-ecommerce-store.herokuapp.com', 'localhost']
 
