91825ac1f89642513366eb2dac14f62d82c56f93
janeyrinn <jane.rinn@gmail.com>
2021-09-11 13:17:33 +0000

    add doc string

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,9 @@
+"""IMPORTS"""
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    """ URL Patterns to open template views"""
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
     path('edit/<item_id>', views.edit_cart, name='edit_cart'),
